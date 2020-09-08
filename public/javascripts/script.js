7bbfd2f1eb991603d0ab2b0fc4e9d149ac6ae5d6
Yvan Menuet <yvan.menuet@gmail.com>
2020-09-08 12:48:02 -0500

    New accessToken for Mapbox

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoieW1lbnVldCIsImEiOiJja2Vrbzd2MzYwM3JiMnFxa2VxaWtzeG1oIn0.LPOW0TTdQPho2P4Tvc2QRA';
+mapboxgl.accessToken = 'pk.eyJ1IjoieW1lbnVldCIsImEiOiJja2V1OGw5emowMXhzMnpudzJzMW9rM2pjIn0.ClSDpO_UqVo2gsMkUjma2w';
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
